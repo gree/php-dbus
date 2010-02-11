022798f94cdfd1099c1e56bfcbf2a0404ba38c07
P. Christeas <p_christ@hol.gr>
2010-02-11 18:48:36 +0200

    Adapt test script to appendArg1(), let it work for notifications.

--- tests/test-connection.php
+++ tests/test-connection.php
@@ -3,24 +3,59 @@
 
 error_reporting(E_ALL);
 
+/** Arguments for cmdline php 
+*/
+function arguments($argv) {
+    $_ARG = array();
+    array_shift($argv); //skip argv[0] !
+    foreach ($argv as $arg) {
+      if (preg_match('/--([^=]+)=(.*)/',$arg,$reg)) {
+        $_ARG[$reg[1]] = $reg[2];
+      } elseif(preg_match('/--([^=]+)/',$arg,$reg)){
+      	$_ARG[$reg[1]] = true;
+      } elseif(preg_match('/^-([a-zA-Z0-9])/',$arg,$reg)) {
+            $_ARG[$reg[1]] = true;
+      } else {
+            $_ARG['input'][]=$arg;
+      }
+    }
+  return $_ARG;
+}
+
+
 $dbus = dbus_bus_get(DBUS_BUS_SESSION);
 
 $m = new DBusMessage(DBUS_MESSAGE_TYPE_METHOD_CALL);
 $m->setDestination("org.kde.VisualNotifications");
-/*
-$m->setPath("/VisualNotifications");
-$m->setInterface("org.kde.VisualNotifications");
-$m->setAutoStart(true);
-$m->setMember("Notify");
-$m->appendArgs("DBus test app",0,0,0,"Summary","Text body",array(),array(),0);
-*/
 
-$m->setPath("/VisualNotifications");
-$m->setInterface("org.freedesktop.DBus.Introspectable");
-$m->setAutoStart(true);
-$m->setMember("Introspect");
+$cli_args = arguments($argv);
+if (empty($cli_args['input']))
+	$cli_args['input'] = array();
 
-echo "a\n";
+switch ($cli_args['input'][0]){
+case 'notify':
+	$m->setPath("/VisualNotifications");
+	$m->setInterface("org.kde.VisualNotifications");
+	$m->setAutoStart(true);
+	$m->setMember("Notify");
+	$m->appendArgs("DBus test app");
+	$m->appendArg1(0, 'u');
+	$m->appendArgs('','',"Summary","Text body", array());
+	$m->appendArg1(array(),'a{sv}');
+	$m->appendArg1(0);
+	break;
+	
+case 'introspect':
+	$m->setPath("/VisualNotifications");
+	$m->setInterface("org.freedesktop.DBus.Introspectable");
+	$m->setAutoStart(true);
+	$m->setMember("Introspect");
+	break;
+default:
+	echo "Please specify the type of test\n";
+	exit(1);
+}
+
 try {
 	$r = $dbus->sendWithReplyAndBlock($m);
 	echo "b\n";
